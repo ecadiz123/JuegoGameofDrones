29814a553f59d93377bf0234a9e965149e3cdc18
Esteban <estebancadiz@gmail.com>
2024-09-15 18:30:05 +0000

    ahora datos en vez de usar listas y filas, se guardan en matriz para mas facil lectura

--- GameOfDrone.cs
+++ GameOfDrone.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 /**
- * Auto-generated code below aims at helping you parse
+
  * the standard input according to the problem statement.
  **/
 class Player
@@ -18,15 +18,18 @@ class Player
         public int y;
 
         //metodo de puntos que da la distancia del pto ingresado al pto en si
-        public int distancia(int xini, int yini)
+        public int distancia(Punto aux)
         {
-            int diag = ((this.x - xini) * (this.x - xini)) + ((this.y - yini) * (this.y - yini));
-            double aux;//doble para aplicar raiz de formula de distancia
-            aux = Math.Sqrt(Convert.ToDouble(diag));//se convierte a double para raiz y se guarda en aux
+            int diag = ((this.x - aux.x) * (this.x - aux.x)) + ((this.y - aux.y) * (this.y - aux.y));//es int porque los puntos son int
+            double diagdouble;//doble para aplicar raiz de formula de distancia
+
+            diagdouble = Math.Sqrt(Convert.ToDouble(diag));//se convierte a double para raiz y se guarda en diagdouble
+
             //Se redondea manualmente para obtener el valor mas cercano
             //ya que metodo convert redondea a valor par
-            Math.Round(aux);
-            return Convert.ToInt32(aux);
+            Math.Round(diagdouble);
+
+            return Convert.ToInt32(diagdouble);
 
         }
 	//metodo para printear el pto en el formato output del juego
@@ -46,25 +49,26 @@ class Player
         int Z = int.Parse(inputs[3]); // number of zones on the map (4 to 8)
         int X = 0;
         int Y = 0;
-        //Lista donde se va a guardar centros areas
-        List<Punto> CentrosAreas = new List<Punto>();
+        //Arreglo donde se va a guardar centros de areas
+        Punto[] CentrosAreas = new Punto[Z];
         for (int i = 0; i < Z; i++)//Z es cantidad de zonas
         {
             //aux para guardar centros
-            Punto centrolist = new Punto();
+            Punto centroarreglo = new Punto();
             inputs = Console.ReadLine().Split(' ');
             X = int.Parse(inputs[0]); // corresponds to the position of the center of a zone. A zone is a circle with a radius of 100 units.
             Y = int.Parse(inputs[1]);
-            centrolist.x = X;
-            centrolist.y = Y;
-            CentrosAreas.Add(centrolist);
+            centroarreglo.x = X;
+            centroarreglo.y = Y;
+	    CentrosAreas[i] = centroarreglo;
         }
-        //Fila para drones propios
-        Queue<Punto> MisDrones = new Queue<Punto>();
-        //Se va a hacer Lista con equipos enemigos que contenga fila
-        //donde cada fila tendra los drones de estos
-        List<Queue<Punto>> EquiposEnemigos = new List<Queue<Punto>>();
-        Queue<Punto> DronesEnemigos = new Queue<Punto>();
+
+
+	//Matriz que guardara posiciones de drones con sus respectivos equipos
+	//fila=equipo
+	//columna=dron
+	//recordar que con valor variable ID es drones propios, es decir Equipos[ID,0] es dron 0 propio
+	Punto[,] Equipos = new Punto[P,D];
 
         // game loop
         while (true)
@@ -73,37 +77,18 @@ class Player
             {
                 int TID = int.Parse(Console.ReadLine()); // ID of the team controlling the zone (0, 1, 2, or 3) or -1 if it is not controlled. The zones are given in the same order as in the initialization.
             }
-            for (int i = 0; i < P; i++)//i define con que jugador se esta trabajando en el ciclo
+            for (int i = 0; i < P; i++)//i define con que jugador se esta trabajando en el ciclo ID es propio
             {
                 for (int j = 0; j < D; j++)//D es cantidad de drones
                 {
                     inputs = Console.ReadLine().Split(' ');
                     int DX = int.Parse(inputs[0]); // The first D lines contain the coordinates of drones of a player with the ID 0, the following D lines those of the drones of player 1, and thus it continues until the last player.
                     int DY = int.Parse(inputs[1]);
-                    //caso donde se trabaja con drones propios, se van a guardar en fila
-                    if (i == ID)
-                    {
-                        //aux para guardar drones
-                        Punto auxdron = new Punto();
-                        auxdron.x = DX;
-                        auxdron.y = DY;
-                        MisDrones.Enqueue(auxdron);
-                    }
-                    else
-                    {
-                        //Se guardan drones del enemigo en fila
-                        Punto auxdron2 = new Punto();
-                        auxdron2.x = DX;
-                        auxdron2.y = DY;
-                        DronesEnemigos.Enqueue(auxdron2);
-                    }
-                }
-                //Guardado de fila de drones enemigos en lista de los equipos
-                if (i != ID)
-                {
-                    EquiposEnemigos.Add(DronesEnemigos);
-                    //Ahora se limpia drones enemigos para el siguiente enemigo
-                    DronesEnemigos.Clear();
+		    //se van a guardar drones en matriz equipos
+		    Punto aux1 = new Punto();
+		    aux1.x= DX;
+		    aux1.y= DY;
+		    Equipos[i,j] = aux1;
                 }
             }
 
--- obj/Debug/net8.0/InteligenciaArtificial.AssemblyInfo.cs
+++ obj/Debug/net8.0/InteligenciaArtificial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InteligenciaArtificial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf1a50b0c89293f388487122460840d3904fc61")]
 [assembly: System.Reflection.AssemblyProductAttribute("InteligenciaArtificial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InteligenciaArtificial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
